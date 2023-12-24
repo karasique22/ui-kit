0f4f0b17160b7a01fddeae0bbeb44d51c0594076
carasique22 <timofeidudin@mail.ru>
2023-12-24 14:58:56 +0300

    add empty input label

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
+function debounce(func, delay) {
+    let timeoutId;
+    return function() {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(func, delay);
+    };
+}
+
 const inputs = document.querySelectorAll('input[type="text"]');
 
 inputs.forEach((input) => {
-	input.addEventListener("input", () => {
-		const errorLabel = input.nextElementSibling;
+    input.addEventListener("input", debounce(() => {
+        const errorLabel = input.nextElementSibling;
+        let errorMessage = "";
 
-		if (input.value !== "4" && input.value !== "") {
-			input.classList.add("error");
-			input.classList.remove("correct");
+        if (input.value !== "4" && input.value !== "") {
+            errorMessage = "Способ исправить ошибку";
+        } else if (input.value === "") {
+            errorMessage = "Не заполнено поле";
+        }
 
-			if (!errorLabel || !errorLabel.classList.contains("visible")) {
-				const errorLabel = document.createElement("label");
-				errorLabel.textContent = "Способ исправить ошибку";
-				errorLabel.setAttribute("for", input.id);
-				input.insertAdjacentElement("afterend", errorLabel);
-				setTimeout(() => {
-					errorLabel.classList.add("visible");
-				}, 10);
-			}
-		} else {
-			input.classList.add("correct");
-			input.classList.remove("error");
-
-			if (errorLabel && errorLabel.classList.contains("visible")) {
-				errorLabel.classList.remove("visible");
-				setTimeout(() => {
-					errorLabel.remove();
-				}, 300);
-			}
+		if (errorLabel && errorLabel.classList.contains("visible")) {
+			errorLabel.classList.remove("visible");
+			setTimeout(() => {
+				errorLabel.remove();
+			}, 300);
 		}
 
-		if (input.value === "") {
-			input.classList.remove("error");
-			input.classList.remove("correct");
+        if (errorMessage !== "") {
+            input.classList.add("error");
+            input.classList.remove("correct");
 
-			if (errorLabel && errorLabel.classList.contains("visible")) {
-				errorLabel.classList.remove("visible");
-				setTimeout(() => {
-					errorLabel.remove();
-				}, 300);
-			}
-		}
-	});
+            const newErrorLabel = document.createElement("label");
+            newErrorLabel.textContent = errorMessage;
+            newErrorLabel.setAttribute("for", input.id);
+            input.insertAdjacentElement("afterend", newErrorLabel);
+            setTimeout(() => {
+                newErrorLabel.classList.add("visible");
+            }, 10);
+        } else {
+            input.classList.add("correct");
+            input.classList.remove("error");
+        }
+    }, 300));
 });
