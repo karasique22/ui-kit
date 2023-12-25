503a0ba9480b8f3d557caf3d364594bfefe218ed
carasique22 <timofeidudin@mail.ru>
2023-12-25 21:44:35 +0300

    pop-up

--- script.js
+++ script.js
@@ -1,45 +1,194 @@
+// inputs state
+
 function debounce(func, delay) {
-    let timeoutId;
-    return function() {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(func, delay);
-    };
+	let timeoutId;
+	return function () {
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(func, delay);
+	};
 }
 
-const inputs = document.querySelectorAll('input[type="text"]');
+const inputs = document.querySelectorAll(".input-text");
 
 inputs.forEach((input) => {
-    input.addEventListener("input", debounce(() => {
-        const errorLabel = input.nextElementSibling;
-        let errorMessage = "";
+	input.addEventListener(
+		"input",
+		debounce(() => {
+			const errorLabel = input.nextElementSibling;
+			let errorMessage = "";
 
-        if (input.value !== "4" && input.value !== "") {
-            errorMessage = "Способ исправить ошибку";
-        } else if (input.value === "") {
-            errorMessage = "Не заполнено поле";
-        }
+			if (input.value !== "4" && input.value !== "") {
+				errorMessage = "Способ исправить ошибку";
+			} else if (input.value === "") {
+				errorMessage = "Не заполнено поле";
+			}
 
-		if (errorLabel && errorLabel.classList.contains("visible")) {
-			errorLabel.classList.remove("visible");
-			setTimeout(() => {
-				errorLabel.remove();
-			}, 300);
-		}
+			if (errorLabel && errorLabel.classList.contains("visible")) {
+				errorLabel.classList.remove("visible");
+				setTimeout(() => {
+					errorLabel.remove();
+				}, 300);
+			}
 
-        if (errorMessage !== "") {
-            input.classList.add("error");
-            input.classList.remove("correct");
+			if (errorMessage !== "") {
+				input.classList.add("error");
+				input.classList.remove("correct");
 
-            const newErrorLabel = document.createElement("label");
-            newErrorLabel.textContent = errorMessage;
-            newErrorLabel.setAttribute("for", input.id);
-            input.insertAdjacentElement("afterend", newErrorLabel);
-            setTimeout(() => {
-                newErrorLabel.classList.add("visible");
-            }, 10);
-        } else {
-            input.classList.add("correct");
-            input.classList.remove("error");
-        }
-    }, 300));
+				const newErrorLabel = document.createElement("label");
+				newErrorLabel.textContent = errorMessage;
+				newErrorLabel.setAttribute("for", input.id);
+				input.insertAdjacentElement("afterend", newErrorLabel);
+				setTimeout(() => {
+					newErrorLabel.classList.add("visible");
+				}, 10);
+			} else {
+				input.classList.add("correct");
+				input.classList.remove("error");
+			}
+		}, 300)
+	);
+});
+
+// popup open-close
+
+function onClickSort1() {
+	const popup = document.getElementById("popup1");
+	popup.classList.toggle("visible");
+}
+
+function onClickClose1() {
+	const popup = document.getElementById("popup1");
+	popup.classList.remove("visible");
+}
+
+function onClickSort2() {
+	const popup = document.getElementById("popup2");
+	popup.classList.toggle("visible");
+}
+
+function onClickClose2() {
+	const popup = document.getElementById("popup2 ");
+	popup.classList.remove("visible");
+}
+
+// popup tags generation
+
+function getRandomPopularity() {
+	return Math.floor(Math.random() * 10) + 1;
+}
+
+const tags = [
+	{
+		id: 1,
+		name: "гранж",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 2,
+		name: "оверсайз",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 3,
+		name: "винтаж",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 4,
+		name: "аксессуары",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 5,
+		name: "панк",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 6,
+		name: "дрейн",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 7,
+		name: "спортик",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 8,
+		name: "классика",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 9,
+		name: "эксперт",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 10,
+		name: "новичок",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 11,
+		name: "продвинутый",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 12,
+		name: "экология",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 13,
+		name: "эксперименты",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 14,
+		name: "бренды",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 15,
+		name: "тренды",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 16,
+		name: "материалы",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 17,
+		name: "эко-подход",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 18,
+		name: "советы",
+		popularity: getRandomPopularity(),
+	},
+	{
+		id: 19,
+		name: "личный опыт",
+		popularity: getRandomPopularity(),
+	},
+];
+
+const popupTagsElements = document.querySelectorAll(".popup-tags");
+
+tags.sort((a, b) => b.popularity - a.popularity);
+
+let lastTagIndex = 0;
+
+popupTagsElements.forEach((popupTagsElement) => {
+    const numberOfTags = Math.floor(Math.random() * 9) + 7;
+    for (let i = lastTagIndex; i < numberOfTags + lastTagIndex; i++) {
+        const tag = document.createElement("button");
+        tag.classList.add("popup-tag");
+        tag.classList.add("btn-tertiary1");
+        tag.textContent = tags[i].name;
+        popupTagsElement.appendChild(tag);
+    }
+    lastTagIndex += numberOfTags;
 });
